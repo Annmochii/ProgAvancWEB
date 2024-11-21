36dd7743161478479b6ede648a8806ac212f78e0
Anna Maria Lima <87787347+Annmochii@users.noreply.github.com>
2024-11-20 21:04:31 -0300

    new post rout

--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
 
 import postController from "../controllers/PostController";
+import UserMiddleware from "../middlewares/UserMiddleware";
+
 
 const postRouter = Router();
 
 
 //Listar posts
-postRouter.get("/posts", postController.listPosts);
+postRouter.get("/posts", UserMiddleware.analyseToken , postController.listPosts);
 
 //Inserir posts
-postRouter.post("/post", postController.createPost);
+postRouter.post("/post", UserMiddleware.analyseToken , postController.createPost);
 
 //Atualizar posts
-postRouter.put("/post/:id", postController.updatePost);
+postRouter.put("/post/:id", UserMiddleware.analyseToken , postController.updatePost);
 
 //Deletar posts
-postRouter.delete("/post/:id", postController.deletePost);
+postRouter.delete("/post/:id", UserMiddleware.analyseToken , postController.deletePost);
+
+//Buscar posts
+postRouter.get("/post/:id", UserMiddleware.analyseToken , postController.getPost);
 
 export default postRouter;
